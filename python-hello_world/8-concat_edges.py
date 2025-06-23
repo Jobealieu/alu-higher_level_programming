b1ca785992b39f86e14ebfdd299c23d526e4052f
Jobealieu <a.jobe@alustudent.com>
2025-06-22 17:35:36 -0700

    Add 8-concat_edges.py script

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[116:120] + str[0:6]
-print(str)
+str = "Python - Lecture: Object-Oriented Programming with Python"
+print(str[39:66] + str[112:118])
+
+
