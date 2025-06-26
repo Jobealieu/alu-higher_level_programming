73fc726e61279b92b0d14cb2d2b0dc978a55c3c2
Jobealieu <a.jobe@alustudent.com>
2025-06-26 14:52:27 -0700

    Add 9-multiply_by_2 script

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    """Returns a new dictionary with all values multiplied by 2"""
-    if not a_dictionary:
-        return {}
-    
     new_dict = {}
     for key, value in a_dictionary.items():
         new_dict[key] = value * 2
-    
     return new_dict
