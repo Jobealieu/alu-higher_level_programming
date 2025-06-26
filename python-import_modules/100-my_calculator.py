ba3c489d1beed75b0a9c906870e9c40178aa3578
Jobealieu <a.jobe@alustudent.com>
2025-06-26 08:40:59 -0700

    Add 100-my_calculator.py script

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,17 +3,17 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    
+
     # Check if exactly 3 arguments are provided (excluding script name)
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     # Get the arguments
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-    
+
     # Check if operator is valid and perform calculation
     if operator == '+':
         result = add(a, b)
@@ -26,6 +26,6 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    
+
     # Print the result in the required format
     print("{} {} {} = {}".format(a, operator, b, result))
