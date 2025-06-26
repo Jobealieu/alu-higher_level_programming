649573cd9e518085f4d694c1b9d68826006936d1
Jobealieu <a.jobe@alustudent.com>
2025-06-26 15:17:21 -0700

    Add 100-weight_average.py script

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,8 +2,8 @@
 def weight_average(my_list=None):
     if not my_list:
         return 0
-    
+
     total_weighted_score = sum(score * weight for score, weight in my_list)
     total_weight = sum(weight for score, weight in my_list)
-    
+
     return total_weighted_score / total_weight
