869ee6357fa08c6e23ae36ed7b3d66d371c5236a
Jobealieu <a.jobe@alustudent.com>
2025-06-26 14:58:52 -0700

    Add 10-best_score script

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,13 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    
     best_key = None
     best_value = None
-    
     for key, value in a_dictionary.items():
         if best_value is None or value > best_value:
             best_key = key
             best_value = value
-    
     return best_key
