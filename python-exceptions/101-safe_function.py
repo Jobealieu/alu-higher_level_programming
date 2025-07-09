fbe96e76e14867c34f64e313f29b1c6be2914cdc
Jobealieu <a.jobe@alustudent.com>
2025-07-09 16:47:50 -0700

    Add 101-safe_function.py script

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 
 def safe_function(fct, *args):
-    """
-    Executes a function safely and handles any exceptions.
-    
-    Args:
-        fct: The function to execute
-        *args: Arguments to pass to the function
-    
-    Returns:
-        The result of the function if successful, None if an exception occurs
-    """
     try:
         return fct(*args)
     except Exception as e:
+        import sys
         print("Exception: {}".format(e), file=sys.stderr)
         return None
