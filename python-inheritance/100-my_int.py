ca894308c056d6121e38648825769e5c049e3aa8
Jobealieu <a.jobe@alustudent.com>
2025-07-10 00:27:48 -0700

    Add 100-int.py script

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -6,11 +6,11 @@ MyInt class that inherits from int with inverted == and != operators
 
 class MyInt(int):
     """A rebel integer class with inverted equality operators"""
-    
+
     def __eq__(self, other):
         """Override equality operator to return inverted result"""
         return super().__ne__(other)
-    
+
     def __ne__(self, other):
         """Override not-equal operator to return inverted result"""
         return super().__eq__(other)
