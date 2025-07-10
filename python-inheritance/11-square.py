03cd37e1446d5bc19d9183e38e411dcf60d8c459
Jobealieu <a.jobe@alustudent.com>
2025-07-10 00:10:14 -0700

    Add 11-square.py script

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,21 +1,19 @@
-#!/usr/bin/python3
-"""Define a Square class that inherits from Rectangle."""
-Rectangle = __import__('9-rectangle').Rectangle
+# 11-square.py
+
+from rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """A Square class that inherits from Rectangle."""
-
     def __init__(self, size):
-        """Initialize a Square with size.
-
-        Args:
-            size (int): The size of the square (both width and height).
-        """
+        """Initialize a Square instance."""
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
 
     def __str__(self):
-        """Return the square description in the format [Square] <width>/<height>."""
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        """Return the string representation of the square."""
+        return f"[Square] {self.__size}/{self.__size}"
