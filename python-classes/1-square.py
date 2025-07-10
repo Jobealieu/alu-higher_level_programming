1f61e98b59e743fb24d6eea9eadc84a9ddd48175
Jobealieu <a.jobe@alustudent.com>
2025-07-09 17:29:17 -0700

    Add 1-square.py script

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,10 +4,10 @@
 
 class Square:
     """A class that defines a square"""
-    
+
     def __init__(self, size):
         """Initialize a new Square instance
-        
+
         Args:
             size: The size of the square
         """
