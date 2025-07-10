9e5f9a7dd2edf78d6354ae57ca07a5c780abeba6
Jobealieu <a.jobe@alustudent.com>
2025-07-09 22:00:17 -0700

    Add 100-magic_string.py script

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("BestSchool")
-    return ", ".join(l)
+def magic_string(l=[0]):
+    l[0] += 1
+    return ", ".join(["BestSchool"] * l[0])
