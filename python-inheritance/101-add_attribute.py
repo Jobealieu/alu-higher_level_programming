bf7bd4e6106ff0458cf436f722b0839bb5310936
Jobealieu <a.jobe@alustudent.com>
2025-07-10 00:32:32 -0700

    Add 101-add_attribute.py script

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -16,16 +16,13 @@ def add_attribute(obj, attr, value):
     Raises:
         TypeError: If the object can't have new attributes
     """
-    # Check if object has __dict__ attribute (most common case)
     if hasattr(obj, '__dict__'):
         setattr(obj, attr, value)
         return
-    
-    # Check if object has __slots__ and the attribute is in slots
+
     if hasattr(obj, '__slots__'):
         if attr in obj.__slots__:
             setattr(obj, attr, value)
             return
-    
-    # If we get here, the object can't have new attributes
+
     raise TypeError("can't add new attribute")
