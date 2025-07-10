39c172ad2c1bbb476fa1f499a580dd54385187d5
Jobealieu <a.jobe@alustudent.com>
2025-07-09 17:34:12 -0700

    Add 2-square.py script

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,13 +4,13 @@
 
 class Square:
     """A class that defines a square by its size with validation"""
-    
+
     def __init__(self, size=0):
         """Initialize a new Square instance
-        
+
         Args:
             size (int): The size of the square (default is 0)
-            
+
         Raises:
             TypeError: If size is not an integer
             ValueError: If size is less than 0
