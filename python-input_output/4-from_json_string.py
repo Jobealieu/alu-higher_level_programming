a871363710ac435d27440be03e85f99d7d2d8d18
Jobealieu <a.jobe@alustudent.com>
2025-07-16 16:03:54 -0700

    Add 4-from_json_string.py script

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines a function to convert JSON string to Python object"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string
+    
+    Args:
+        my_str: JSON string to convert
+        
+    Returns:
+        Python object represented by the JSON string
+    """
+    return json.loads(my_str)
