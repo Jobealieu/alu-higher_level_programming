bceb0ce6cbddefaec809888a6234f492548027ce
Jobealieu <a.jobe@alustudent.com>
2025-07-16 16:00:55 -0700

    Add 3-to_json_string.py script

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,13 +4,5 @@ import json
 
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object (string)
-
-    Args:
-        my_obj: The object to be converted to JSON string
-
-    Returns:
-        str: JSON string representation of the object
-    """
+    """Returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
-
