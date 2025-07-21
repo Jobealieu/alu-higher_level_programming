c3d335c23e3bb6f2b1a698be437db5faf54afe34
Jobealieu <a.jobe@alustudent.com>
2025-07-21 07:10:18 -0700

    Add 101-avg_temperatures.sql script

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
--- Script that displays the top 3 of cities temperature during July and August
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-WHERE month = 'July' OR month = 'August'
-GROUP BY city
-ORDER BY avg_temp DESC
+-- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+SELECT city, AVG(value) AS avg_temp 
+FROM temperatures 
+WHERE month IN (7, 8) 
+GROUP BY city 
+ORDER BY avg_temp DESC 
 LIMIT 3;
