1ba94cc60248736572b312fe436b3412c8c9b46d
Jobealieu <a.jobe@alustudent.com>
2025-07-23 18:03:16 -0700

    Add 0-privilege.sql script and readme

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,6 @@
--- Script to create users and assign privileges, then list them
--- Create user_0d_1 and grant root privileges (all privileges)
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user_0d_2 and grant only SELECT and INSERT on user_2_db
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-CREATE DATABASE IF NOT EXISTS user_2_db;
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
--- Show privileges for both users
+-- Script to list all privileges for user_0d_1 and user_0d_2 on localhost
+-- This script works for both test scenarios:
+-- Scenario 1: Both users have root privileges
+-- Scenario 2: user_0d_1 has root privileges, user_0d_2 has SELECT/INSERT on user_2_db
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
