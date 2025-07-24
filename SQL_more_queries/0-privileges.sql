0790d6bb9634788e14957c74016dd189d3b472e4
Jobealieu <a.jobe@alustudent.com>
2025-07-23 18:00:36 -0700

    Add 0-privilege.sql script and readme

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,13 @@
--- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+-- Script to create users and assign privileges, then list them
+-- Create user_0d_1 and grant root privileges (all privileges)
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Create user_0d_2 and grant only SELECT and INSERT on user_2_db
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS user_2_db;
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+
+-- Show privileges for both users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
