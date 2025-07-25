55f09f56e6a18b772a9189adac315ceae77bf1e3
Jobealieu <a.jobe@alustudent.com>
2025-07-24 22:20:18 -0700

    Add 0-hbtn_status.py script

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""A script that fetches https://intranet.hbtn.io/status using urllib"""
+"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
