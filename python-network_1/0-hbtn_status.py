1ebf244fa4371f146d688f7ba219c39f4e7c67b6
Jobealieu <a.jobe@alustudent.com>
2025-07-24 21:48:06 -0700

    Add 0-hbtn_status.py script

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alu-intranet.hbtn.io/status"
 
     with urllib.request.urlopen(url) as response:
         body = response.read()
