61d9d6ab9b44cd3632543650f8316484bd0b2098
Jobealieu <a.jobe@alustudent.com>
2025-07-24 23:04:17 -0700

    Add 7-error_code.py script

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -8,9 +8,9 @@ print: Error code: followed by the value of the HTTP status code
 import requests
 import sys
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
-    
     response = requests.get(url)
     
     if response.status_code >= 400:
