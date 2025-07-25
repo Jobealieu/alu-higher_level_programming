2e3a6548b6c604b9f581933fc9f8954ce4da41a3
Jobealieu <a.jobe@alustudent.com>
2025-07-25 03:39:08 +0000

    Add 102-catch_me.sh

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Follow redirects with custom headers
-curl -s -L -H "X-School: 98" -X PUT 0.0.0.0:5000/catch_me
+# This script makes a request to catch the server and get "You got me!" response
+curl -s -X DELETE "0.0.0.0:5000/catch_me"
