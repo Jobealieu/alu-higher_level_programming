c10b0487e0a4ad1bdbe354024f2133d88d03b801
Jobealieu <a.jobe@alustudent.com>
2025-07-25 03:32:07 +0000

    Add 102-catch_me.sh

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Try with a specific catch header
-curl -s -X POST -H "X-School: Holberton" -d "name=catch_me" 0.0.0.0:5000/catch_me
+# Follow redirects and try different method
+curl -s -L -X PUT 0.0.0.0:5000/catch_me
