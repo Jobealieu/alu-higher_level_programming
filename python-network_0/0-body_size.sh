930f5ba31c22ee35731d1618d3a7708e09159269
Jobealieu <a.jobe@alustudent.com>
2025-07-25 02:13:58 +0000

    Add 0-body_size.sh script and README

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the responsecurl -sI "$1" | grep -i Content-Length | cut -d' ' -f2
+# Script that takes a URL and displays the size of the response body in bytes
+curl -sI "$1" | grep -i content-length | cut -d' ' -f2 | tr -d '\r'
