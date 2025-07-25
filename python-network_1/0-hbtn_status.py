efd0678321c5160cbdb744483c6e9cf125f7752c
Jobealieu <a.jobe@alustudent.com>
2025-07-24 23:54:57 -0700

    Add 0-hbtn_status.py script

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,14 +2,15 @@
 """
 0-hbtn_status.py
 
-Fetches https://alu-intranet.hbtn.io/status using urllib
+Fetches http://0.0.0.0:5050/status using urllib
 and displays the body response in a formatted way.
 """
 
 import urllib.request
 
+
 def main():
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "http://0.0.0.0:5050/status"
     # Using with statement to open the URL and get the response object
     with urllib.request.urlopen(url) as response:
         body = response.read()
@@ -18,7 +19,8 @@ def main():
     print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
 
 if __name__ == "__main__":
     main()
