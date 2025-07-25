796979dce44ff685dc32e381c64d27bc5513cb92
Jobealieu <a.jobe@alustudent.com>
2025-07-24 23:19:18 -0700

    Add 10-my_github.py script

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -2,17 +2,15 @@
 """
 Script that takes GitHub credentials and uses GitHub API to display user id
 """
-
 import requests
 import sys
-
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    
+
     url = "https://api.github.com/user"
-    
+
     try:
         response = requests.get(url, auth=(username, password))
         if response.status_code == 200:
