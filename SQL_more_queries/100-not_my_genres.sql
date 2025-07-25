8eca84fbe4cf094401e64099c3613cf5ed44779d
Jobealieu <a.jobe@alustudent.com>
2025-07-24 17:10:29 -0700

    Add 100-not_my_genres.sql script

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -2,10 +2,9 @@
 -- Script that lists all shows without the genre Comedy
 SELECT tv_shows.title
 FROM tv_shows
-WHERE tv_shows.id NOT IN (
-    SELECT tv_show_genres.show_id
-    FROM tv_show_genres
-    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_genres.name = 'Comedy'
-)
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name IS NULL OR tv_genres.name != 'Comedy'
+GROUP BY tv_shows.title
+HAVING SUM(tv_genres.name = 'Comedy') = 0
 ORDER BY tv_shows.title ASC;
