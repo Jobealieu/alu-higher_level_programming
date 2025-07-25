37f139439dac44e802e629619314216d9e2255a7
Jobealieu <a.jobe@alustudent.com>
2025-07-24 17:12:24 -0700

    Add 100-not_my_genres.sql script

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,11 @@
--- 18. No Comedy tonight!
--- Script that lists all shows without the genre Comedy
-SELECT tv_shows.title
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name IS NULL OR tv_genres.name != 'Comedy'
-GROUP BY tv_shows.title
-HAVING SUM(tv_genres.name = 'Comedy') = 0
-ORDER BY tv_shows.title ASC;
+-- 17. Not my genre
+-- Script to list all genres not linked to the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN (
+    SELECT tv_show_genres.genre_id
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
