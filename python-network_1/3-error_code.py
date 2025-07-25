5612e90ba12b5c3e7e37afd4b7d6563f45fc94f2
Jobealieu <a.jobe@alustudent.com>
2025-07-24 22:47:48 -0700

    Add 3-error_code.py script

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -4,14 +4,14 @@ Script that takes in a URL, sends a request to the URL and displays the body
 of the response. Handles urllib.error.HTTPError exceptions.
 """
 
-import urllib.request
-import urllib.error
 import sys
+import urllib.error
+import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
+
     try:
         with urllib.request.urlopen(url) as response:
             body = response.read().decode('utf-8')
