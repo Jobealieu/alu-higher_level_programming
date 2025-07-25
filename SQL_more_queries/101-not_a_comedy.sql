47576b2787331865ac15e165ada7d33f44851797
Jobealieu <a.jobe@alustudent.com>
2025-07-24 17:21:17 -0700

    Add 101-not_a_comedy.sql script

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -5,10 +5,13 @@
 
 SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.genre_id != (
-    SELECT id
-    FROM tv_genres
-    WHERE name = 'Comedy'
-) OR tv_show_genres.genre_id IS NULL
+WHERE tv_shows.id NOT IN (
+    SELECT tv_show_genres.tv_show_id
+    FROM tv_show_genres
+    WHERE tv_show_genres.genre_id = (
+        SELECT id
+        FROM tv_genres
+        WHERE name = 'Comedy'
+    )
+)
 ORDER BY tv_shows.title;
