5259600884f8f34795e993e587776ba43ff819f2
Jobealieu <a.jobe@alustudent.com>
2025-07-24 22:00:25 -0700

    Add 0-hbtn_status.py script

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Script that fetches https://alu-intranet.hbtn.io/status using urllib"""
-
+"""Script that fetches a URL status"""
+import sys
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = sys.argv[1] if len(sys.argv) > 1 else "https://alu-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
