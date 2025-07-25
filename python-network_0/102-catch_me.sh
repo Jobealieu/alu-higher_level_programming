2a137759961316d3a53f3df0ef57d274bcb650fb
Jobealieu <a.jobe@alustudent.com>
2025-07-25 04:02:42 +0000

    Add 102-catch_me.sh

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that makes a request to catch_me endpoint
-curl -s -X POST 0.0.0.0:5000/catch_me
+# Script that makes a request to catch_me endpoint to get "You got me!" response
+curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
