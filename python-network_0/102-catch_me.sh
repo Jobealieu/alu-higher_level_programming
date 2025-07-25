c77f65dd2005b19d20735810d3e3901f4c214c32
Jobealieu <a.jobe@alustudent.com>
2025-07-25 04:01:02 +0000

    Add 102-catch_me.sh

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that catches the server response
-curl -s -X PUT -H "X-Custom-Header: catch" 0.0.0.0:5000/catch_me
+# Script that makes a request to catch_me endpoint
+curl -s -X POST 0.0.0.0:5000/catch_me
