f6210b37a40ce2301c0e6297faff62d1fcec21c2
Jobealieu <a.jobe@alustudent.com>
2025-08-05 10:36:34 +0200

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with N from the database"""
+"""Script that lists all states with a name starting with N"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
-    
-    # Create cursor object
     cursor = db.cursor()
-    
-    # Execute SQL query to select states starting with 'N'
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    
-    # Fetch all results
-    results = cursor.fetchall()
-    
-    # Print results
-    for row in results:
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
-    
-    # Close cursor and database connection
     cursor.close()
     db.close()
