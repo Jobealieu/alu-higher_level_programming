d78e5e2d5ad324afb0c233b783b745541f9cf3e1
Jobealieu <a.jobe@alustudent.com>
2025-08-05 10:39:55 +0200

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
-
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    
-    # Connect to MySQL server
+    # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
     
-    # Create cursor
+    # Create cursor object
     cursor = db.cursor()
     
-    # Execute query to get all states sorted by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Execute query to get states starting with 'N'
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     
-    # Fetch all results
+    # Fetch all matching records
     results = cursor.fetchall()
     
-    # Display results
+    # Print each result
     for row in results:
         print(row)
     
