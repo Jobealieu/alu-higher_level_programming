8bc9dcf5cebbe2901253a11b2d6bb74dceaee98b
Jobealieu <a.jobe@alustudent.com>
2025-09-29 08:10:50 -0700

    Add 10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = process.argv;
+function factorial(n) {
+  const num = Number.parseInt(n, 10);
+  if (Number.isNaN(num)) return 1;
+  if (num <= 1) return 1;
+  return num * factorial(num - 1);
+}
+console.log(factorial(argv[2]));
