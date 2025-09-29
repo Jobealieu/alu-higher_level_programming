f0594b6d059ed8a8d95ec7139fd0d49595bbed5d
Jobealieu <a.jobe@alustudent.com>
2025-09-29 08:08:10 -0700

    Add 9-add.js script

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function add (a, b) {
-  return a + b
+  return a + b;
 }
 
-const first = parseInt(process.argv[2], 10)
-const second = parseInt(process.argv[3], 10)
+const first = parseInt(process.argv[2], 10);
+const second = parseInt(process.argv[3], 10);
 
-console.log(add(first, second))
+console.log(add(first, second));
