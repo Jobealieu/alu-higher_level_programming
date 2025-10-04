174cf500c5cc2fe29953b19086d0cb1a53f5bea0
Jobealieu <a.jobe@alustudent.com>
2025-10-04 07:23:32 -0700

    Add 100-starwars_characters.js script

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const film = JSON.parse(body);
+    const characters = film.characters;
+    characters.forEach(characterUrl => {
+      request(characterUrl, (err, res, charBody) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const character = JSON.parse(charBody);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
